2b2c34ac9a9131b6c782e18d6cddfe0b9cb77db4
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-11 02:26:48 +0300

    fix

--- Jannerlord/Jannerlord.cs
+++ Jannerlord/Jannerlord.cs
@@ -1,11 +1,19 @@
-﻿using TaleWorlds.MountAndBlade;
+﻿using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
 
 namespace Jannerlord{
 
-    public class JannerlordSubModule : MBSubModuleBase{
+    public class JannerlordSubModule : MBSubModuleBase {
 
         protected override void OnSubModuleLoad(){
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+                new TextObject("Message"),
+                9990,
+                () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+                false));
         }
+        
     }
 
 }
