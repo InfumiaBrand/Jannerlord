f6552686b2690f247fb04c8aea97b0ebc8f4889a
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-10 21:35:57 +0300

    fix

--- src/Jannerlord/JannerlordSubModule.cs
+++ src/Jannerlord/JannerlordSubModule.cs
@@ -1,7 +1,15 @@
-﻿using TaleWorlds.*;
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using Module = TaleWorlds.MountAndBlade.Module;
 
 ﻿namespace Jannerlord {
-	 public partial class JannerlordSubModule{
+	 
+	 [PublicAPI]
+	 public partial class JannerlordSubModule : MBSubModuleBase {
+		 
+		 
 		 
 	 }
 
